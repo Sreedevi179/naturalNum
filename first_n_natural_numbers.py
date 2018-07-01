9b988a6fd0e8491949349c417abb962377ee02ca
t3ds <40736940+t3ds@users.noreply.github.com>
2018-07-01 20:20:52 +0530

    Add files via upload

---
+++ first_n_natural_numbers.py
@@ -0,0 +1,26 @@
+#(sum of n1 natural numbers+n2 natural numbers....)
+
+i=0
+num=[]
+sum1=0
+import array
+print('Enter the number of parameters (n1,n2...) : ')
+n=input()
+
+print('Enter the parameters(Enter nothing to stop) : ')
+
+while True:
+    parameters=input()
+
+    if parameters=='':
+        break
+    num=num+[parameters]
+
+for parameters in num:
+    sum=int(parameters)*(int(parameters)+1)/2
+    sum1=sum1+sum
+
+print('the answer is',sum1)
+    
+    
+    
